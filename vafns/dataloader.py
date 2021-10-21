9d78fcd28a1dfe3ebf2dcdb7fb2b366b98ad7f2a
beloslavamalakova <beloslava.malakova@gmail.com>
2021-10-21 15:59:34 +0300

    abstract class

--- vafns/dataloader.py
+++ vafns/dataloader.py
@@ -1,12 +1,27 @@
+#for the kaggle dataset 
 import torch
 import torchvision
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
 import numpy as np
 import math
 
-#Dataset - location; initializing, reading the dataset with numpy
-##the first column is the time (dates)
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+torch.backends.cudnn.benchmark = True
 
+#abstract class
+class Dataset(object):
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __add__(self, other):
+        return ConcatDataset([self, other])
+
+
+#Dataset - location; initializing, reading the dataset with numpy
 class YieldCurves(Dataset):
 
     def __init__(self):
@@ -17,11 +32,9 @@ class YieldCurves(Dataset):
         self.y_data=torch.from_numpy(xy[:, [0]]) #n_samples, 1
 
     def __getitem__(self, index):
-
         return self.x_data[index], self.y_data[index]
 
     def __len__ (self):
-
         return self.n_samples
 
 #creating the dataset
@@ -31,7 +44,5 @@ dataset = YieldCurves()
 first_data =dataset[0]
 features, labels = first_data
 print(features, labels)
-
-
 
 #create the dataloader !do not forget -- shuffle=False
