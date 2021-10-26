3ff33abc64f0e3bf10389aacdc4b4c00b87b64ed
beloslavamalakova <beloslava.malakova@gmail.com>
2021-10-27 02:30:24 +0300

    progress

--- vafns/dataloader.py
+++ vafns/dataloader.py
@@ -2,8 +2,8 @@ import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
 from   sklearn.preprocessing  import StandardScaler
-import numpy as np
 import pandas as pd
+import torch.nn as nn
 import math
 import matplotlib as plt
 
@@ -12,7 +12,7 @@ device = torch.device("cuda:0" if use_cuda else "cpu")
 
 class YieldCurves(Dataset):
 
-    def __init__(self, t): #to decide the agrs
+    def __init__(self, t): #TODO to figure out the agrs
         super.__init__()
 
         kaggle= pd.read_csv("../kaggle.csv", parse_dates=['date'], index_col='date')
@@ -25,17 +25,20 @@ class YieldCurves(Dataset):
         self.y=(dataset[[1-2]:, :])
         self.t=t
 
+        x_train = self.x
+        y_train = StandardScaler.transform(self.y)
+
     def __getitem__(self, index):
         return self.x[index], self.y[index]
 
-    def __getitem__(self, ):
-        return self.
-
     def __len__ (self):
-        return self.
+        return self.kaggle
 
-    def time(self, t):
-        labels==inputs[t+1]
+    def __getitem__(self, index):
+        if index + self.t <= len():
+           return self.y_train[index : index + self.t], self.x_train[index : index + self.t]
+        else:
+           raise TypeError("Index must be within the range of t")
 
 dataset = YieldCurves()
 dataset.head(3)
@@ -45,15 +48,15 @@ def main():
     device: torch.device
 
     plt.figure(figsize=(11,4), dpi= 80)
-    pd.plotting.autocorrelation_plot(data.loc['1997-03-01': '2019-09-01', 'Yield Curves'])
-    
+    pd.plotting.autocorrelation_plot(dataset.loc['1997-03-01': '2019-09-01', '1yr':'30YR'])
+
     # outlier process- filtering out the top and bottom 2% of the data; init
+    
+    criterion = nn.CrossEntropyLoss()
+    loss = criterion() #the args in the parenthesis
+    print(loss)
 
-
-
-# cross-entropy= default function 
-
-# splitting the dataset 90/10, train_test_split-- shuffle=False
+    # splitting the dataset 90/10, train_test_split-- shuffle=False
 
 if __name__ == "__main__":
     main()
