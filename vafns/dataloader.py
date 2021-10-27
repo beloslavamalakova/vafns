66b992dfb557a32a04d00830192b29e44b5124a7
beloslavamalakova <beloslava.malakova@gmail.com>
2021-10-28 00:55:16 +0300

    d

--- vafns/dataloader.py
+++ vafns/dataloader.py
@@ -4,6 +4,7 @@ from   sklearn.preprocessing  import StandardScaler
 import pandas as pd
 import torch.nn as nn
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 class YieldCurves(Dataset):
@@ -22,29 +23,29 @@ class YieldCurves(Dataset):
         yields=self.yields
         yields=kaggle[['Date'-'30YR']:, :]
 
-        #new datasets
-        
-        #std y_train = StandardScaler.transform(self.y)
-
-        # num_predictions -- how many steps ahead to predict --1 or 2; predictionite-the following num predictions
         num_predictions=1
-        num_predictions=num_predictions+1
 
-        #default value test=false, if true another dataset, train_test_split
         if test == False:
             return 
         else:
-            return 
+            return #if true another dataset?
+
+        #new datasets
+
+        #TODO outlier process- filtering out the top and bottom 2% of the data
+
+        #std y_train = StandardScaler.transform(self.y)
+
 
     def __len__ (self):
         return self.len(self.yields-self.t-self.num_predictions)
 
-    #getitem ot index do index pkus t; index +t+num_predictions+1 -- x pyrwite t; 
     def __getitem__(self, index):
         if index + self.t < len(self.yields):
-           return self.y_train[index : index + self.t], self.x_train[index : index + self.t]
+           return self.yields[index: index + self.t + self.num_predictions +1]
         else:
            raise TypeError("Index must be within the range of t")
+
 
 def main():
 
@@ -55,12 +56,11 @@ def main():
 
     plt.figure(figsize=(11,4), dpi= 80)
     pd.plotting.autocorrelation_plot(dataset.loc['1997-03-01': '2019-09-01', '1yr':'30YR'])
-
-    #TODO outlier process- filtering out the top and bottom 2% of the data; init
-    
+ 
     criterion = nn.CrossEntropyLoss()
-    loss = criterion() #the args in the parenthesis-- output target -- test, train
+    loss = criterion() #output target -- test, train
     print(loss)
+
 
 if __name__ == "__main__":
     main()
