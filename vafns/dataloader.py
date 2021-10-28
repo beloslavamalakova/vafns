05cea2557152c7767b54497b812945d1ce61744a
beloslavamalakova <beloslava.malakova@gmail.com>
2021-10-29 00:39:09 +0300

    g

--- vafns/dataloader.py
+++ vafns/dataloader.py
@@ -11,24 +11,37 @@ class YieldCurves(Dataset):
     def __init__(self, num_predictions, test, t): 
         super.__init__()
 
-        kaggle= pd.read_csv("../kaggle.csv", parse_dates=['date'], index_col='date')
-        kaggle['Date'] = pd.to_datetime(kaggle['Date'])
-        kaggle = kaggle.set_index('Date')
-
         self.num_predictions=num_predictions
         self.test=test
         self.t=t
 
-        self.yields=kaggle[['Date'-'30YR']:, :]
+        kaggle= pd.read_csv("../kaggle.csv", parse_dates=['date'], index_col='date')
+        kaggle['Date'] = pd.to_datetime(kaggle['Date'])
+        kaggle = kaggle.set_index('Date')
+
+        yields=kaggle[['Date'-'30YR']:, :]
+
+        train=yields[:int(len(self.yields)*0.95)]
+        validation=yields[-int(len(self.yields)*0.05):]
+
+        train=StandardScaler.transform(train)
+        validation=StandardScaler.transform(validation)
 
         num_predictions=1
 
-        self.train=self.yields[:int(len(self.yields)*0.95)]
-        self.validation=self.yields[-int(len(self.yields)*0.05):]
+        min_value=0
+        max_value=1
+        torch.clamp(train, min_value, max_value)
 
-        train=StandardScaler.transform(self.train)
-        validation=StandardScaler.transform(self.validation)
+        x= 
+        y= 
 
+        train_seq_len = t
+        target_seq_len = q
+        for i in range[]: 
+            return (x, y)
+
+        # function default values
 
         if test == False:
             return 
@@ -38,14 +51,16 @@ class YieldCurves(Dataset):
         #TODO outlier process- filtering out the top and bottom 2% of the data
 
 
-
+#train set, test set, x values, target values
 
     def __len__ (self):
         return self.len(self.yields-self.t-self.num_predictions)
 
     def __getitem__(self, index):
         if index + self.t < len(self.yields):
+            #return with torch.tensor
            return self.yields[index: index + self.t + self.num_predictions +1]
+           #why the type error
         else:
            raise TypeError("Index must be within the range of t")
 
@@ -60,6 +75,7 @@ def main():
     plt.figure(figsize=(11,4), dpi= 80)
     pd.plotting.autocorrelation_plot(dataset.loc['1997-03-01': '2019-09-01', '1yr':'30YR'])
  
+    #criterion -- target values , x values
     criterion = nn.CrossEntropyLoss()
     loss = criterion()
     print(loss)
