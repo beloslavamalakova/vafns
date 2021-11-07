af27e8d297648f0c3a4ef4b43f57d89749c5d3e6
beloslavamalakova <beloslava.malakova@gmail.com>
2021-11-07 16:51:43 +0200

    minor updates to the code

--- vafns/data.py
+++ vafns/data.py
@@ -8,16 +8,24 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 class Fed(Dataset):
-    def __init__(self):
+    def __init__(self, num_predictions, test, t):
+
+        self.num_predictions=num_predictions
+        self.test=test
+        self.t=t
+
+
         def clean(x):
             if isinstance(x, str):
                 return normalize('NFKC', x).strip()
             else:
                 return x
 
+
         yields= pd.read_html("https://www.treasury.gov/resource-center/data-chart-center/interest-rates/pages/TextView.aspx?data=yieldAll", match='30 yr')
         yields = yields.applymap(clean)
         yields.columns = yields.columns.to_series().apply(clean)
+
 
         col_type = {
             'Date': 'string',
@@ -35,17 +43,26 @@ class Fed(Dataset):
             '30 yr': 'float',
             }
 
+
         def cleaning(yields):
             clean_dict = {'%': '', '−': '-', '\(est\)': ''}
             yields= yields.replace(clean_dict, regex=True).replace({'N/A': np.nan}).astype(col_type)
 
+
+
+        train=yields[:int(len(self.yields)*0.95)]
+        validation=yields[-int(len(self.yields)*0.05):]
+
+        train=StandardScaler.transform(train)
+        validation=StandardScaler.transform(validation)
         
+
+    def __len__(self):
+        return self.len(self.yields-self.t-self.num_predictions)
 
     def __getitem__(self):
         pass
 
-    def __len__(self):
-        pass
 
 
 def main():
@@ -53,10 +70,6 @@ def main():
     device = torch.device("cuda:0" if use_cuda else "cpu")
     
     dataset= Fed()
-    yields.head()
-
-
-
 
 if __name__ == "__main__":
     main()
